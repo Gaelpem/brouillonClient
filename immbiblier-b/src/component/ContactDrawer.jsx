f367e6017fb0316535e62db7c371140d6ae4bfb8
Gaelpem <pembelegael@gmail.com>
2025-08-08 14:49:45 +0200

    6eme

--- immbiblier-b/src/component/ContactDrawer.jsx
+++ immbiblier-b/src/component/ContactDrawer.jsx
@@ -1,69 +1,55 @@
-import React, {  useRef } from 'react';
+import React, {  useRef, useState } from 'react';
 import {createPortal} from 'react-dom'
 import classes from './ContactDrawer.module.css'
 
 const ContactDrawer = ({onClose}) => {
     // on cree 3 ref pour accéder à des element du DOM 
 
-    
+    const [error,  setError] = useState(false)
+
     const prenomRef = useRef()
-
-
     const nomRef = useRef()
-
-
     const emailRef = useRef()
 
     function handleSend(e){
         e.preventDefault()
     
-   
-       
        // on met le focus sur prenom car c'est le premier input
        prenomRef.current.focus()
-       nomRef.current.focus()
-       emailRef.current.focus()
-       
+ 
          //  On récupère les valeurs des champs
        const prenom = prenomRef.current.value
-        const nom = nomRef.current.value
- 
-    const email = emailRef.current.value
+       const nom = nomRef.current.value
+       const email = emailRef.current.value
+     
+       if(!prenom || !nom || !email){
+          setError(true)
+       }
 
     console.log( "Nom", nom);
     console.log("Prenom", prenom);
     console.log("email", email);
     }
   
-
-
      return createPortal(
     
         <div className={classes.transparentDrop}>
-
-
-
-        <aside className={classes.contact}>
-              
-              <div className={classes.btnContact}>
-              <h1>Contact-</h1>
-
-              <button onClick={onClose}>X</button>
-
-              </div>
-           
-
-           <h2>Une demande particulère?<span className={classes.changeColor}> Parlons-en.</span></h2>
-        
-          
-
-        <div className={classes.inputContainer}>
-
     
+        <aside className={classes.contact}>
+
+
+            <div className={classes.btnEspace}>
+            <div></div>
+        <button onClick={onClose}>X</button>
+               </div>
+               <h1>Contact-</h1>
+               <h2>Une demande particulère?<span className={classes.changeColor}> Parlons-en.</span></h2>
+               <div className={classes.inputContainer}>
 
             <label htmlFor="prenom">Prénom</label>
 
             <input type="text"  ref={prenomRef} id="prenom" />
+
 
             <label htmlFor="nom">Nom</label>
 
@@ -80,6 +66,7 @@ const ContactDrawer = ({onClose}) => {
                <div></div>
                <button onClick={handleSend}>Envoyer</button>
                </div>
+               {error && <p style={{color : "red"}}>Vous devez remplir tous les champs avant d'envoyer</p>}
         </aside>
 
 
