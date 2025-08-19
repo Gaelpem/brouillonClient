ea5e2355083b34abb13de65e7720539b19432cf6
Gaelpem <pembelegael@gmail.com>
2025-08-19 17:16:28 +0200

    miseAjour5

--- immbiblier-b/src/component/BiensDrawer.jsx
+++ immbiblier-b/src/component/BiensDrawer.jsx
@@ -10,18 +10,18 @@ const BiensDrawer = ({ bien, onClose }) => {
 
         <header className={classes.header}>
           <h1>Shtib.</h1>
-          <button onClick={onClose}>X</button>
+          <button onClick={onClose}>[X]</button>
         </header>
 
         <section className={classes.imgDescription}>
+
           <div className={classes.image}>
-            {/* on utilise les données du bien sélectionné */}
-            <img src={bien.image} alt={bien.location} />
+            <img src={bien.image}/>
           </div>
 
           <div className={classes.description}>
             <h1>Description</h1>
-            <p>{bien.description}</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae dolorem nihil excepturi aut suscipit deleniti, tempora quaerat in dolore facere error laboriosam.</p>
           </div>
         </section>
   
